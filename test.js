7942c25c2adb7df3412849092a06d2de7c31a7de
Anargyres <anargyres9278@gmail.com>
2019-03-14 16:46:36 +0100

    update ttest

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 import test from 'ava';
 var axios = require('axios');
+var nock = require('nock');
+
+const scope = nock('https://geocode.xyz')
+  .get('/paris?json=1')
+  .reply(200, {
+    Request: {
+      latt: '0,00',
+      name: 'paris',
+      longt: '1,11',
+    },
+  })
 
 test('form on index', async t => {
   const response = await axios.get("http://localhost:3000");
@@ -12,4 +23,13 @@ test('form on index', async t => {
     } catch (e) {
       t.fail("Une erreur s'est produite");
     }
+});
+
+test('display city name', async t => {
+  const response = await axios.post("https://geocode.xyz/paris?json=1");
+  try {
+    
+  } catch (e) {
+
+  }
 });
